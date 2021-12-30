0901a996f59878e6f37f7ec6e7bb562d26706d44
Nori <noriaki.nakamura@gmail.com>
2021-12-30 13:28:38 +0900

    1st deploy

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,116 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="hello" @click="on_start">{{ msg }}</div>
+    <div class="ind">出力先選択</div>
+    <button 
+      v-for="out_dev in out_infos"
+      :key=out_dev.deviceId
+      @click="select_out(out_dev)"
+    >{{ out_dev.label }}</button>
+    <div class="ind">入力先選択</div>
+    <button 
+      v-for="in_dev in in_infos"
+      :key=in_dev.deviceId
+      @click="select_in(in_dev)"
+    >{{ in_dev.label }}</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      msg: "",
+      audio: null,
+      audioCtx: null,
+      stream: null,
+      out_infos: [],
+      in_infos: []
+    }
+  },
+  mounted() {
+    this.msg = "開始";
+    
+  },
+  methods: {
+    async on_start() {
+      if (this.msg == "開始") {
+        this.msg = "終了";
+        try {
+          this.audioCtx = new AudioContext();
+          this.audio = new Audio();
+
+          this.stream = await navigator.mediaDevices.getUserMedia({ video: false, audio: true });
+
+          const devices = await navigator.mediaDevices.enumerateDevices();
+          this.out_infos = devices.filter(info => {
+            return info.kind == "audiooutput" && info.deviceId != "default" && info.deviceId != "communications" 
+          });
+          console.dir(this.out_infos);
+          this.in_infos = devices.filter(info => {
+            return info.kind == "audioinput" && info.deviceId != "default" && info.deviceId != "communications" 
+          });
+          console.dir(this.in_infos);
+
+          const src = this.audioCtx.createMediaStreamSource(this.stream);
+          const dst = this.audioCtx.createMediaStreamDestination();
+          src.connect(dst);
+          this.audio.srcObject = dst.stream;
+          await this.audio.play();
+
+        } catch (err) {
+          alert(err);
+        }
+      } else {
+        this.msg = "開始";
+        this.stream.getAudioTracks()[0].stop();
+        this.stream = null;
+      }
+    },
+    async select_out(dev) {
+      await this.audio.setSinkId(dev.deviceId);
+    },
+    async select_in(dev) {
+      console.log(dev);
+      try {
+        this.audio.pause();
+        this.audio.srcObject = null;
+        this.stream = await navigator.mediaDevices.getUserMedia({
+          video: false,
+          audio: {
+            deviceId: dev.deviceId
+          }
+        });
+        const src = this.audioCtx.createMediaStreamSource(this.stream);
+        const dst = this.audioCtx.createMediaStreamDestination();
+        src.connect(dst);
+        this.audio.srcObject = dst.stream;
+        await this.audio.play();
+      } catch (err) {
+        alert(err);
+      }
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
+.hello {
+  font-size: 5rem;
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.ind {
+  margin-top: 10px;
+  font-size: 2rem;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+button {
+  font-size: 1rem;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  width: 80%;
+  margin: 5px;
 }
 </style>
